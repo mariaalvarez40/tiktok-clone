a92ce25a75cc4bcfdb50ec0e76fd9206fbe20b35
mariaalvarez40 <malvarez@rightpoint.com>
2020-10-01 14:36:15 -0500

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0H0bj8s6vsh8HdTNmRSaM1XR5umBD9AY",
+  authDomain: "tiktok-clone-98cd3.firebaseapp.com",
+  databaseURL: "https://tiktok-clone-98cd3.firebaseio.com",
+  projectId: "tiktok-clone-98cd3",
+  storageBucket: "tiktok-clone-98cd3.appspot.com",
+  messagingSenderId: "687917115991",
+  appId: "1:687917115991:web:9d8847ccb68d6528049112",
+  measurementId: "G-E21J92KZNW"
+};
